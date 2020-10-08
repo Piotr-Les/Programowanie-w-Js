7fa3f6682864efbafa4b3f699b37c9359e0211b1
Piotr Leś <->
2020-10-08 14:58:45 +0200

    selected all imgs by data- atrib, modal pop up and modal pop down finished

--- cw1-lightbox/app.js
+++ cw1-lightbox/app.js
@@ -1,1 +1,24 @@
-function a() {}
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const targetImg = document.querySelector('.target-img');
+const gtargets = document.querySelectorAll('[data-gtarget]');
+const b1 = document.querySelectorAll('.b1');
+const b2 = document.querySelectorAll('.b2');
+
+gtargets.forEach((gtarget) => {
+	gtarget.addEventListener('click', (e) => {
+		modal.classList.add('active');
+		overlay.classList.add('active');
+		targetImg.src = e.target.src;
+	});
+});
+// b1.addEventListener("click" ()=>{
+//   targetImg.src =
+// })
+
+modal.addEventListener('click', (e) => {
+	if (e.target.classList.contains('modal')) {
+		modal.classList.remove('active');
+		overlay.classList.remove('active');
+	}
+});
