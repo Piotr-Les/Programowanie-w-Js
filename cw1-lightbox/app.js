fb054841df98fbc4af736a64e17a9beaa5f8f2da
Piotr Leś <->
2020-10-13 15:23:55 +0200

    next button working

--- cw1-lightbox/app.js
+++ cw1-lightbox/app.js
@@ -7,20 +7,24 @@ const b2 = document.querySelector('.b2');
 
 gtargets.forEach((gtarget) => {
 	gtarget.addEventListener('click', () => {
+
+		let data = parseInt( gtarget.dataset.gtarget);
 		modal.classList.add('active');
 		overlay.classList.add('active');
-		let data = gtarget.dataset.gtarget;
 		targetImg.src = `https://picsum.photos/400/200?random=${data}`;
-
+		// previous button
 		b1.addEventListener('click', () => {
 			data==1 ? data=1 : data=data-1;
 			targetImg.src = `https://picsum.photos/400/200?random=${data}`;
 		});
-		
+		// next button
+		b2.addEventListener('click', () => {
+			data==6 ? data=6 : data=data+1;
+			targetImg.src = `https://picsum.photos/400/200?random=${data}`;
+		});
 	});
+	
 });
-
-
 
 modal.addEventListener('click', (e) => {
 	if (e.target.classList.contains('modal')) {
