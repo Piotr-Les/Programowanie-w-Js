c0a624b016a46bd4a2e80eba774342de6fc203d6
Piotr Leś <->
2020-11-06 20:15:19 +0100

    keyObj class separated

---
+++ cw2 - drumkit/keyObj.js
@@ -0,0 +1,22 @@
+class keyObj {
+	constructor(selector) {
+		this.uiElement = selector;
+		this.keySoundId = selector.dataset.sound;
+		this.keyLetter = selector.textContent;
+	}
+	getAudioTag(id) {
+		return document.getElementById(id);
+	}
+	animateUiElement(element) {
+		element.classList.add('playing');
+		element.addEventListener('transitionend', () => {
+			element.classList.remove('playing');
+		});
+	}
+	playSound() {
+		const keySound = this.getAudioTag(this.keySoundId);
+		keySound.currentTime = 0;
+		keySound.play();
+		this.animateUiElement(this.uiElement);
+	}
+}
--- cw2 - drumkit/main.js
+++ cw2 - drumkit/main.js
@@ -1,26 +1,3 @@
-class keyObj {
-	constructor(selector) {
-		this.uiElement = selector;
-		this.keySoundId = selector.dataset.sound;
-		this.keyLetter = selector.textContent;
-	}
-	getAudioTag(id) {
-		return document.getElementById(id);
-	}
-	animateUiElement(element) {
-		element.classList.add('playing');
-		element.addEventListener('transitionend', () => {
-			element.classList.remove('playing');
-		});
-	}
-	playSound() {
-		const keySound = this.getAudioTag(this.keySoundId);
-		keySound.currentTime = 0;
-		keySound.play();
-		this.animateUiElement(this.uiElement);
-	}
-}
-
 const keys = document.querySelectorAll('.key');
 let objArr = [];
 keys.forEach((keyItem) => {
