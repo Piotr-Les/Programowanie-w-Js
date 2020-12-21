2c38e55bb8551e4dcc4bd109227ad887c5e18d8b
Piotr Leś <->
2020-12-21 17:33:18 +0100

    added GameOver screen and option to restart game

--- cw4-ballGame/main.js
+++ cw4-ballGame/main.js
@@ -1,23 +1,23 @@
 const canvas = document.getElementById('canv');
 let ctx = canvas.getContext('2d');
+let animationFrame;
+let seconds;
+let minutes;
 
 const player = new Ball(15, 20, 20, 'purple');
 let holes;
 initHoles();
 function initHoles() {
 	holes = [];
-	for (let i = 0; i < 20; i++) {
-		let radius = 20;
+	for (let i = 0; i < 15; i++) {
+		let radius = randFromRange(8, 25);
 		let x = randFromRange(radius, window.innerWidth - radius);
 		let y = randFromRange(radius, window.innerHeight - radius);
 		let color = '#FF8066';
 
 		if (i != 0) {
 			for (let j = 0; j < holes.length; j++) {
-				if (
-					Sphere.getDistance(x, y, holes[j].x, holes[j].y) - radius * 2.5 <
-					0
-				) {
+				if (Sphere.getDistance(x, y, holes[j].x, holes[j].y) - radius * 3 < 0) {
 					x = randFromRange(radius, window.innerWidth - radius);
 					y = randFromRange(radius, window.innerHeight - radius);
 					j = -1;
@@ -27,7 +27,7 @@ function initHoles() {
 		holes.push(new Hole(radius, x, y, color, i));
 	}
 }
-
+let start = Date.now();
 function updateCanvas() {
 	clearCanvas();
 	//init the PLAYER
@@ -49,8 +49,51 @@ function updateCanvas() {
 			}
 		}
 	});
-	requestAnimationFrame(updateCanvas);
+	displayTime();
+	animationFrame = requestAnimationFrame(updateCanvas);
+	//game over condition
+	if (holes.length === 0) {
+		gameOver();
+		return;
+	}
 
+	function displayTime() {
+		seconds = Math.floor((Date.now() - start) / 1000);
+		minutes = Math.floor(seconds / 60);
+		seconds %= 60;
+		ctx.font = '20px Arial';
+		ctx.fillStyle = 'red';
+		ctx.fillText(
+			minutes.toString().padStart(2, '0') +
+				':' +
+				seconds.toString().padStart(2, '0'),
+			10,
+			25
+		);
+	}
+	function gameOver() {
+		displayFinalScore();
+		cancelAnimationFrame(animationFrame);
+		const restart = document.querySelector('.restart');
+		restart.addEventListener('click', () => {
+			window.location.reload();
+		});
+	}
+
+	function displayFinalScore() {
+		const final = document.querySelector('.final-box');
+		final.classList.add('active');
+		insertScore();
+
+		function insertScore() {
+			let scoreValue = `
+			${minutes.toString().padStart(2, '0')} : 
+			${seconds.toString().padStart(2, '0')}`;
+			const scoreCont = document.querySelector('.score');
+
+			scoreCont.innerHTML = `Your time is ${scoreValue}`;
+		}
+	}
 	function clearCanvas() {
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 	}
